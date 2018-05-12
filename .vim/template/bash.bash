e1d9edf79fff5d17b7706551f774bf68839bebc3
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2018-05-13 02:14:36 +0900

    .vim: bash template: Add setopt -s nullglob failglob

--- .vim/template/bash.bash
+++ .vim/template/bash.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -u
-set -o pipefail
-shopt -s lastpipe
+set -o pipefail || exit $?	## bash 3.0+
+shopt -s lastpipe || exit $?	## bash 4.2+
+shopt -s nullglob || exit $?	## bash 2.0+
+#shopt -s failglob || exit $?	## bash 3.0+
 
