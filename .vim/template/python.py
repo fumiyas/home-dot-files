bf592201b8e9e838b4087bba59d5412b52300397
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2018-05-13 02:15:22 +0900

    .vim: Python template: Add more common snippets

--- .vim/template/python.py
+++ .vim/template/python.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
-## -*- encoding: utf-8 -*- vim:shiftwidth=4
+## -*- coding: utf-8 -*- vim:shiftwidth=4:expandtab:
 
+from __future__ import print_function
+
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def main(argv):
+    logger.warning("warning log test")
+    print(argv)
+
+    return 0
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logformatter = logging.Formatter('%(filename)s: %(levelname)s: %(message)s')
+    loghandler = logging.StreamHandler()
+    loghandler.setlevel(logging.INFO)
+    loghandler.setFormatter(logformatter)
+    logger.addHandler(loghandler)
+
+    if len(sys.argv) < 2:
+        print("Usage: %s STRING [...]" % (sys.argv[0]), file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(main(sys.argv[1:]))
