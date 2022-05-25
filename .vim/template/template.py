4eee593d8b9b5990a683fe31dcd7a47b731cbe0c
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2022-05-25 14:38:35 +0900

    vim: template.py: Use Python 3 and add more examples

--- .vim/template/template.py
+++ .vim/template/template.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ## -*- coding: utf-8 -*- vim:shiftwidth=4:expandtab:
 
 from __future__ import print_function
@@ -6,23 +6,31 @@ from __future__ import print_function
 import logging
 import sys
 
+
 logger = logging.getLogger(__name__)
 
 
 def main(argv):
     logger.warning("warning log test")
+    logger.info("info log test")
+    try:
+        undefined_func()
+    except:
+        logger.error("Boo!", exc_info=True)
+
     print(argv)
 
     return 0
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    logformatter = logging.Formatter('%(filename)s: %(levelname)s: %(message)s')
-    loghandler = logging.StreamHandler()
-    loghandler.setlevel(logging.INFO)
-    loghandler.setFormatter(logformatter)
-    logger.addHandler(loghandler)
+    logging.basicConfig(
+        level=logging.WARN,
+        format=(sys.argv[0] + ': %(levelname)s: %(message)s'),
+    )
+
+    ## Set a specific log level to a module
+    logging.getLogger("urllib3").setLevel(logging.DEBUG)
 
     if len(sys.argv) < 2:
         print("Usage: %s STRING [...]" % (sys.argv[0]), file=sys.stderr)
