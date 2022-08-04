a00445441b9687272cbe7f11ec7c4cce1e401902
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2022-08-04 11:09:27 +0900

    vim: template.py: Add SyslogHandler

--- .vim/template/template.py
+++ .vim/template/template.py
@@ -4,29 +4,44 @@
 from __future__ import print_function
 
 import logging
+import logging.handlers
+import os
 import sys
-
 
 logger = logging.getLogger(__name__)
 
 
 def main(argv):
+    print(argv)
     logger.warning("warning log test")
     logger.info("info log test")
     try:
         undefined_func()
-    except:
-        logger.error("Boo!", exc_info=True)
-
-    print(argv)
+    except Exception as e:
+        logger.error("error log test with trace: %s" % (e), exc_info=True)
 
     return 0
 
 
 if __name__ == '__main__':
+    logging_handlers = []
+    if True:
+        logging_handlers.append(logging.StreamHandler())
+    if True:
+        syslog_handler = logging.handlers.SysLogHandler(
+            address='/dev/log',
+            facility='user',
+        )
+        syslog_handler.ident = f'{os.path.basename(sys.argv[0])}[{os.getpid()}]: '
+
+        logging_handlers.append(
+            syslog_handler
+        )
+
     logging.basicConfig(
         level=logging.WARN,
-        format=(sys.argv[0] + ': %(levelname)s: %(message)s'),
+        format=(f'{sys.argv[0]}: %(levelname)s: %(message)s'),
+        handlers=logging_handlers,
     )
 
     ## Set a specific log level to a module
