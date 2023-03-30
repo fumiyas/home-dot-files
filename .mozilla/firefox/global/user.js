f2284ba57760da93eba0c907d5834009b1c7e5d7
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2023-03-30 10:51:42 +0900

    Firefox: Update user.js

--- .mozilla/firefox/global/user.js
+++ .mozilla/firefox/global/user.js
@@ -1,15 +1,27 @@
 // ln -s ../global/user.pref ./profilename/user.pref
 
 // http://www.hackermusings.com/2012/05/firefoxs-graphics-performance-on-x11/
-user_pref("gfx.xrender.enabled", false);
+//user_pref("gfx.xrender.enabled", false);
+
+// キャレット (文字入力エリア内のカーソル) の点滅速度
+user_pref("ui.caretBlinkTime", 250);
 
 // URL バーの URL を省略表示しない
 user_pref("browser.urlbar.trimURLs", false);
+// URL バーのドロップダウンメニューに表示する URL の数
+user_pref("browser.urlbar.maxRichResults", 30);
+// URL バーの入力補完を自動的に適用しない
+user_pref("browser.urlbar.autoFill", false);
+// URL バーにフォーカスを移したときにドロップダウンメニューを表示しない
+// [Privacy & Search] Category > [Address Bar] Clause > [Shortcuts] Check
+// (↓キーで開く)
+user_pref("browser.urlbar.suggest.topsites", false);
 
 // URL バーのデザインを Firefox 74 以前に戻す
 // これでないと履歴の表示幅が狭くなって URL が見切れる。
 // https://www.userchrome.org/megabar-styling-firefox-address-bar.html
-user_pref("browser.urlbar.update1", false);
+// obsolete?
+//user_pref("browser.urlbar.update1", false);
 
 // URL バーでの検索を無効化
 user_pref("keyword.enabled", false);
@@ -26,124 +38,114 @@ user_pref("middlemouse.contentLoadURL", false);
 // 右クリック禁止ページを回避（JavaScript の詳細設定）
 user_pref("dom.event.contextmenu.enabled", false);
 
-// フレームなページで強制的にリサイズ可能に
-user_pref("layout.frames.force_resizability", true);
-
 // 国際化ドメイン名を punycode のまま表示
 user_pref("network.IDN_show_punycode", true);
 
-// Twitter Web UI 表示時の不具合の回避
-// https://twitter.com/d_toybox/status/637088309072625664
-//user_pref("layout.interruptible-reflow.enabled", false);
-
-// 「タブを閉じる」ボタンの表示方法
-// 0:アクティブなタブのみ *1:全てのタブ 2:表示しない 3:タブバーの右端
-user_pref("browser.tabs.closeButtons", 2);
-
-// 検索欄に入力時に表示される検索サイト選択ボタンを非表示
-user_pref("browser.search.showOneOffButtons", false);
+// タブを現在のタブの後に開く
+user_pref("browser.tabs.insertAfterCurrent", true);
+// タブをダブルクリックで閉じる
+user_pref("browser.tabs.closeTabByDblclick", true);
 
 // テキストエリアへの中ボタンクリックでクリップボードでなくセレクションをペースト
-user_pref("dom.event.clipboardevents.enabled", false);
+// This breaks copy-and-paste behavior on Google Docs, Twitter, Facebook
+//user_pref("dom.event.clipboardevents.enabled", false);
 
 // 署名なしの拡張を有効化
-user_pref("xpinstall.signatures.required", false);
-
-//user_pref("layers.acceleration.force-enabled", true);
+//user_pref("xpinstall.signatures.required", false);
 
 // ======================================================================
 
-user_pref("layout.css.devPixelsPerPx", "1.2");
-
-user_pref("font.size.variable.ar",19);
-user_pref("font.size.variable.el",19);
-user_pref("font.size.variable.he",19);
-user_pref("font.size.variable.ja",19);
-user_pref("font.size.variable.ko",19);
-user_pref("font.size.variable.th",19);
-user_pref("font.size.variable.x-armn",19);
-user_pref("font.size.variable.x-beng",19);
-user_pref("font.size.variable.x-cans",19);
-user_pref("font.size.variable.x-cyrillic",19);
-user_pref("font.size.variable.x-devanagari",19);
-user_pref("font.size.variable.x-ethi",19);
-user_pref("font.size.variable.x-geor",19);
-user_pref("font.size.variable.x-gujr",19);
-user_pref("font.size.variable.x-guru",19);
-user_pref("font.size.variable.x-khmr",19);
-user_pref("font.size.variable.x-knda",19);
-user_pref("font.size.variable.x-math",19);
-user_pref("font.size.variable.x-mlym",19);
-user_pref("font.size.variable.x-orya",19);
-user_pref("font.size.variable.x-sinh",19);
-user_pref("font.size.variable.x-tamil",19);
-user_pref("font.size.variable.x-telu",19);
-user_pref("font.size.variable.x-tibt",19);
-user_pref("font.size.variable.x-unicode",19);
-user_pref("font.size.variable.x-western",19);
-user_pref("font.size.variable.zh-CN",19);
-user_pref("font.size.variable.zh-HK",19);
-user_pref("font.size.variable.zh-TW",19);
-
-user_pref("font.size.fixed.ar", 19);
-user_pref("font.size.fixed.el", 19);
-user_pref("font.size.fixed.he", 19);
-user_pref("font.size.fixed.ja", 19);
-user_pref("font.size.fixed.ko", 19);
-user_pref("font.size.fixed.th", 19);
-user_pref("font.size.fixed.x-armn", 19);
-user_pref("font.size.fixed.x-beng", 19);
-user_pref("font.size.fixed.x-cans", 19);
-user_pref("font.size.fixed.x-cyrillic", 19);
-user_pref("font.size.fixed.x-devanagari", 19);
-user_pref("font.size.fixed.x-ethi", 19);
-user_pref("font.size.fixed.x-geor", 19);
-user_pref("font.size.fixed.x-gujr", 19);
-user_pref("font.size.fixed.x-guru", 19);
-user_pref("font.size.fixed.x-khmr", 19);
-user_pref("font.size.fixed.x-knda", 19);
-user_pref("font.size.fixed.x-math", 19);
-user_pref("font.size.fixed.x-mlym", 19);
-user_pref("font.size.fixed.x-orya", 19);
-user_pref("font.size.fixed.x-sinh", 19);
-user_pref("font.size.fixed.x-tamil", 19);
-user_pref("font.size.fixed.x-telu", 19);
-user_pref("font.size.fixed.x-tibt", 19);
-user_pref("font.size.fixed.x-unicode", 19);
-user_pref("font.size.fixed.x-western", 19);
-user_pref("font.size.fixed.zh-CN", 19);
-user_pref("font.size.fixed.zh-HK", 19);
-user_pref("font.size.fixed.zh-TW", 19);
-
-user_pref("font.minimum-size.ar", 0);
-user_pref("font.minimum-size.el", 0);
-user_pref("font.minimum-size.he", 0);
-user_pref("font.minimum-size.ja", 0);
-user_pref("font.minimum-size.ko", 0);
-user_pref("font.minimum-size.th", 0);
-user_pref("font.minimum-size.x-armn", 0);
-user_pref("font.minimum-size.x-beng", 0);
-user_pref("font.minimum-size.x-cans", 0);
-user_pref("font.minimum-size.x-cyrillic", 0);
-user_pref("font.minimum-size.x-devanagari", 0);
-user_pref("font.minimum-size.x-ethi", 0);
-user_pref("font.minimum-size.x-geor", 0);
-user_pref("font.minimum-size.x-gujr", 0);
-user_pref("font.minimum-size.x-guru", 0);
-user_pref("font.minimum-size.x-khmr", 0);
-user_pref("font.minimum-size.x-knda", 0);
-user_pref("font.minimum-size.x-math", 0);
-user_pref("font.minimum-size.x-mlym", 0);
-user_pref("font.minimum-size.x-orya", 0);
-user_pref("font.minimum-size.x-sinh", 0);
-user_pref("font.minimum-size.x-tamil", 0);
-user_pref("font.minimum-size.x-telu", 0);
-user_pref("font.minimum-size.x-tibt", 0);
-user_pref("font.minimum-size.x-unicode", 0);
-user_pref("font.minimum-size.x-western", 0);
-user_pref("font.minimum-size.zh-CN", 0);
-user_pref("font.minimum-size.zh-HK", 0);
-user_pref("font.minimum-size.zh-TW", 0);
+//user_pref("layout.css.devPixelsPerPx", "1.2");
+//
+//user_pref("font.size.variable.ar",19);
+//user_pref("font.size.variable.el",19);
+//user_pref("font.size.variable.he",19);
+//user_pref("font.size.variable.ja",19);
+//user_pref("font.size.variable.ko",19);
+//user_pref("font.size.variable.th",19);
+//user_pref("font.size.variable.x-armn",19);
+//user_pref("font.size.variable.x-beng",19);
+//user_pref("font.size.variable.x-cans",19);
+//user_pref("font.size.variable.x-cyrillic",19);
+//user_pref("font.size.variable.x-devanagari",19);
+//user_pref("font.size.variable.x-ethi",19);
+//user_pref("font.size.variable.x-geor",19);
+//user_pref("font.size.variable.x-gujr",19);
+//user_pref("font.size.variable.x-guru",19);
+//user_pref("font.size.variable.x-khmr",19);
+//user_pref("font.size.variable.x-knda",19);
+//user_pref("font.size.variable.x-math",19);
+//user_pref("font.size.variable.x-mlym",19);
+//user_pref("font.size.variable.x-orya",19);
+//user_pref("font.size.variable.x-sinh",19);
+//user_pref("font.size.variable.x-tamil",19);
+//user_pref("font.size.variable.x-telu",19);
+//user_pref("font.size.variable.x-tibt",19);
+//user_pref("font.size.variable.x-unicode",19);
+//user_pref("font.size.variable.x-western",19);
+//user_pref("font.size.variable.zh-CN",19);
+//user_pref("font.size.variable.zh-HK",19);
+//user_pref("font.size.variable.zh-TW",19);
+//
+//user_pref("font.size.fixed.ar", 19);
+//user_pref("font.size.fixed.el", 19);
+//user_pref("font.size.fixed.he", 19);
+//user_pref("font.size.fixed.ja", 19);
+//user_pref("font.size.fixed.ko", 19);
+//user_pref("font.size.fixed.th", 19);
+//user_pref("font.size.fixed.x-armn", 19);
+//user_pref("font.size.fixed.x-beng", 19);
+//user_pref("font.size.fixed.x-cans", 19);
+//user_pref("font.size.fixed.x-cyrillic", 19);
+//user_pref("font.size.fixed.x-devanagari", 19);
+//user_pref("font.size.fixed.x-ethi", 19);
+//user_pref("font.size.fixed.x-geor", 19);
+//user_pref("font.size.fixed.x-gujr", 19);
+//user_pref("font.size.fixed.x-guru", 19);
+//user_pref("font.size.fixed.x-khmr", 19);
+//user_pref("font.size.fixed.x-knda", 19);
+//user_pref("font.size.fixed.x-math", 19);
+//user_pref("font.size.fixed.x-mlym", 19);
+//user_pref("font.size.fixed.x-orya", 19);
+//user_pref("font.size.fixed.x-sinh", 19);
+//user_pref("font.size.fixed.x-tamil", 19);
+//user_pref("font.size.fixed.x-telu", 19);
+//user_pref("font.size.fixed.x-tibt", 19);
+//user_pref("font.size.fixed.x-unicode", 19);
+//user_pref("font.size.fixed.x-western", 19);
+//user_pref("font.size.fixed.zh-CN", 19);
+//user_pref("font.size.fixed.zh-HK", 19);
+//user_pref("font.size.fixed.zh-TW", 19);
+//
+//user_pref("font.minimum-size.ar", 0);
+//user_pref("font.minimum-size.el", 0);
+//user_pref("font.minimum-size.he", 0);
+//user_pref("font.minimum-size.ja", 0);
+//user_pref("font.minimum-size.ko", 0);
+//user_pref("font.minimum-size.th", 0);
+//user_pref("font.minimum-size.x-armn", 0);
+//user_pref("font.minimum-size.x-beng", 0);
+//user_pref("font.minimum-size.x-cans", 0);
+//user_pref("font.minimum-size.x-cyrillic", 0);
+//user_pref("font.minimum-size.x-devanagari", 0);
+//user_pref("font.minimum-size.x-ethi", 0);
+//user_pref("font.minimum-size.x-geor", 0);
+//user_pref("font.minimum-size.x-gujr", 0);
+//user_pref("font.minimum-size.x-guru", 0);
+//user_pref("font.minimum-size.x-khmr", 0);
+//user_pref("font.minimum-size.x-knda", 0);
+//user_pref("font.minimum-size.x-math", 0);
+//user_pref("font.minimum-size.x-mlym", 0);
+//user_pref("font.minimum-size.x-orya", 0);
+//user_pref("font.minimum-size.x-sinh", 0);
+//user_pref("font.minimum-size.x-tamil", 0);
+//user_pref("font.minimum-size.x-telu", 0);
+//user_pref("font.minimum-size.x-tibt", 0);
+//user_pref("font.minimum-size.x-unicode", 0);
+//user_pref("font.minimum-size.x-western", 0);
+//user_pref("font.minimum-size.zh-CN", 0);
+//user_pref("font.minimum-size.zh-HK", 0);
+//user_pref("font.minimum-size.zh-TW", 0);
 
 // ======================================================================
 
