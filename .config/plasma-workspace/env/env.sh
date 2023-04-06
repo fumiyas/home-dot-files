f780ea2dfa7074fb531ccab366a688e5dd7287c9
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2023-04-06 11:43:06 +0900

    KDE: Add env command for Plasma Workspace

---
+++ .config/plasma-workspace/env/env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+##
+## KDE: Plasma Workspace ~/.config/plasma-workspace/env/env.sh
+##
+
+export PATH="$HOME/bin:$PATH"
+
+syndaemon -d -t
+
+if type wcwidth-cjk >/dev/null 2>&1; then
+  eval "$(wcwidth-cjk --sh-init)"
+fi
