22ddd5ab69998b7cf52ad94bcd884bb708fc4768
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-09-10 22:23:38 +0900

    WezTerm: Set window_padding, initial_cols / rows, adjust font_size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,13 +2,23 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font_size = 17
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 100
+config.initial_rows = 64
+
+config.font_size = 20
 config.font = wezterm.font_with_fallback({
     { family = "Ricty Diminished Discord" },
     { family = "Emoji One", assume_emoji_presentation = true },
 })
 
-config.hide_tab_bar_if_only_one_tab = true
 config.treat_east_asian_ambiguous_width_as_wide = true
+config.bold_brightens_ansi_colors = true
 
 return config
