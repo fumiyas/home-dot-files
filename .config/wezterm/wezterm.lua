9592fb98f8eb9e1d554bf03dd644d1708501e559
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-09-13 13:20:29 +0900

    WezTerm: Set corsor and colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,7 +18,39 @@ config.font = wezterm.font_with_fallback({
     { family = "Emoji One", assume_emoji_presentation = true },
 })
 
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 400
+config.cursor_blink_ease_in = "Linear"
+config.cursor_blink_ease_out = "Linear"
+
 config.treat_east_asian_ambiguous_width_as_wide = true
-config.bold_brightens_ansi_colors = true
+config.bold_brightens_ansi_colors = false
+config.colors = {
+  foreground = 'white',
+  background = 'black',
+  cursor_fg = 'olive',
+  cursor_bg = 'orange',
+  cursor_border = 'orange',
+  ansi = {
+    'black',
+    'red',
+    'lime',
+    'yellow',
+    '#00A0FF', --'blue',
+    'fuchsia',
+    'aqua',
+    'white',
+  },
+  brights = {
+    'grey',
+    'maroon',
+    'green',
+    'olive',
+    '#00C0FF', --'navy',
+    'purple',
+    'teal',
+    'silver',
+  },
+}
 
 return config
