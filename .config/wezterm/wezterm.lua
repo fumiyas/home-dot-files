a343d60d0d2b4fd30c14f07b1ec2a5a5943abc92
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-09-27 09:46:56 +0900

    WezTerm: mouse_bindings: Disable click URL to open
    https://www.reddit.com/r/wezterm/comments/10jda7o/is_there_a_way_not_to_open_urls_on_simple_click/

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -64,4 +64,25 @@ config.colors = {
   },
 }
 
+config.mouse_bindings = {
+  -- Disable the default click behavior
+  {
+    event = { Up = { streak = 1, button = "Left"} },
+    mods = "NONE",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  -- Ctrl+click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+  -- Disable the Ctrl+click down event to stop programs from seeing it when a URL is clicked
+  {
+    event = { Down = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = wezterm.action.Nop,
+  },
+}
+
 return config
