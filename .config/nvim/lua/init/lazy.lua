c5d36dcf9b90fdc179261fc480943ce91f1a2930
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-04-30 23:50:18 +0900

    Nvim: plugin: Add rainbow_csv

--- .config/nvim/lua/init/lazy.lua
+++ .config/nvim/lua/init/lazy.lua
@@ -14,4 +14,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { "nvim-treesitter/nvim-treesitter", event = "CmdlineEnter" },
   { "haya14busa/vim-migemo", event = "CmdlineEnter" },
+  { "mechatroner/rainbow_csv", event = "CmdlineEnter", lazy = false },
 })
