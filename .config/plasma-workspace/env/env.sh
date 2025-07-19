f2bcd68642f19d5cbddcc3d8b00aa2d697b11b38
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-07-19 11:51:55 +0900

    KDE: Adjust environment variables

--- .config/plasma-workspace/env/env.sh
+++ .config/plasma-workspace/env/env.sh
@@ -3,19 +3,37 @@
 ## KDE: Plasma Workspace ~/.config/plasma-workspace/env/env.sh
 ##
 
+set -u
+
 export LANG="ja_JP.UTF-8"
 export PATH="$HOME/bin:$PATH"
 
-if [ -z "$DEBUGINFOD_URLS" ]; then
-    DEBUGINFOD_URLS=$(cat /dev/null "/etc/debuginfod"/*.urls 2>/dev/null | tr '\n' ' ')
-    [ -n "$DEBUGINFOD_URLS" ] && export DEBUGINFOD_URLS || unset DEBUGINFOD_URLS
+if [ -z "${DEBUGINFOD_URLS-}" ]; then
+  DEBUGINFOD_URLS=$(cat /dev/null "/etc/debuginfod"/*.urls 2>/dev/null | tr '\n' ' ')
+  if [ -n "$DEBUGINFOD_URLS" ]; then
+    export DEBUGINFOD_URLS
+  else
+    unset DEBUGINFOD_URLS
+  fi
 fi
 
-xset b off
+export XMODIFIERS=@im=fcitx
 
-touch_dev=$(xinput --list --name-only |grep Finger)
-if [ -n "$touch_dev" ]; then
-  xinput --disable "$touch_dev"
+if [ "${XDG_SESSION_TYPE-}" = wayland ]; then
+  export QT_QPA_PLATFORM="wayland;xcb"
+  export QT_IM_MODULES="wayland;fcitx;ibus"
+  ## FIXME: Broken?
+  #export GTK_IM_MODULES="wayland"
+  export GTK_IM_MODULE="fcitx"
+  export GDK_BACKEND="wayland"
+  export MOZ_ENABLE_WAYLAND="1"
+else
+  xset b off
+  #export GTK_IM_MODULE=fcitx5
+  touch_dev=$(xinput --list --name-only |grep Finger)
+  if [ -n "$touch_dev" ]; then
+    xinput --disable "$touch_dev"
+  fi
 fi
 
 if type wcwidth-cjk >/dev/null 2>&1; then
