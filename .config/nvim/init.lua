1a94294d6e2bc244ddacf2a8ef2114a92a9988ee
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-09-03 22:05:35 +0900

    Nvim: vim.opt.laststatus = 2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.opt.fileencodings = {"UCS-BOM", "UTF-8"}
 vim.opt.ambiwidth = "double"
 vim.opt.history = 10000
 vim.opt.shortmess:append({ I = true })
-vim.opt.laststatus = 3
+vim.opt.laststatus = 2
 vim.opt.mouse = {}
 
 vim.opt.wildmenu = true
